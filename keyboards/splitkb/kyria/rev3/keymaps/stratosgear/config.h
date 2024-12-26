29a8369716bae79c1d5978730ef9e578062d75a6
Stratos Gerakakis <stratos@gerakakis.net>
2024-12-26 17:13:27 +0100

    Migrate from RGBLIGHT to RGBMATRIX

--- keyboards/splitkb/kyria/rev3/keymaps/stratosgear/config.h
+++ keyboards/splitkb/kyria/rev3/keymaps/stratosgear/config.h
@@ -16,23 +16,44 @@
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    ifndef RGBLIGHT_LIMIT_VAL
-#    	define RGBLIGHT_LIMIT_VAL 150
-#    endif
+#ifdef RGB_MATRIX_ENABLE
+
+// https://docs.qmk.fm/features/rgblight#keycodes
+// Do not share keycodes between RGBMATRIX and RGBLIGHT
+// #define RGB_MATRIX_DISABLE_SHARED_KEYCODES
+
+// #define ENABLE_RGB_MATRIX_PIXEL_FLOW
+
+// RGB Matrix Effect Typing Heatmap
+#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+
+// This effect will color the RGB matrix according to a heatmap of recently
+// pressed keys. Whenever a key is pressed its "temperature" increases as
+// well as that of its neighboring keys. The temperature of each key is
+//then decreased automatically every 25 milliseconds by default.
+
+// In order to change the delay of temperature decrease define
+// RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS:
+// #define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
+
+// As heatmap uses the physical position of the leds set in the g_led_config,
+// you may need to tweak the following options to get the best effect for
+//your keyboard. Note the size of this grid is 224x64.
+
+// Limit the distance the effect spreads to surrounding keys.
+// #define RGB_MATRIX_TYPING_HEATMAP_SPREAD 40
+
+// Limit how hot surrounding keys get from each press.
+// #define RGB_MATRIX_TYPING_HEATMAP_AREA_LIMIT 16
+
+// Remove the spread effect entirely.
+// #define RGB_MATRIX_TYPING_HEATMAP_SLIM
+
+// It's also possible to adjust the tempo of heating up. It's defined as the
+// number of shades that are increased on the HSV scale. Decreasing this value
+// increases the number of keystrokes needed to fully heat up the key.// c
+// #define RGB_MATRIX_TYPING_HEATMAP_INCREASE_STEP 32
+
 #endif
 
 #ifdef OLED_DRIVER_ENABLE
--- keyboards/splitkb/kyria/rev3/keymaps/stratosgear/rules.mk
+++ keyboards/splitkb/kyria/rev3/keymaps/stratosgear/rules.mk
@@ -1,5 +1,5 @@
-SRC += features/select_word.c
 SRC += features/achordion.c
+SRC += features/select_word.c
 
 # Convert from Elite-C to standard RP2040
 # CONVERT_TO=promicro_rp2040
@@ -10,9 +10,17 @@ OLED_ENABLE = yes
 
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 
+# Enabling backlight fails, with a request to set BACKLIGHT_PIN, that I do not know
+# I believe Kyria does NOT have a single backlight but an array of colored leds
 # BACKLIGHT_ENABLE = yes
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+RGB_MATRIX_ENABLE = yes
+
+# Seems to be working without a driver
+# RGB_MATRIX_DRIVER = ws2812
+
+# Enabling this compiles, but fails to perform correctly with "snow" on the  OLED display
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 
 TAP_DANCE_ENABLE = yes
 
