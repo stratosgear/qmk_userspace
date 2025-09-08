f728d1b91be22b806c5f5ed115f70b6c75c5e942
Stratos Gerakakis <stratos@gerakakis.net>
2025-09-08 10:18:57 +0200

    Kerberos: Adde OLED display

--- keyboards/kerberos/config.h
+++ keyboards/kerberos/config.h
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
+// #include "quantum.h"
+
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
 #define SERIAL_USART_FULL_DUPLEX
@@ -17,3 +19,22 @@
 // #define ENCODER_DIRECTION_FLIP
 #define ENCODER_RESOLUTION 4
 #define ENCODER_DEFAULT_POS 0x3
+
+
+// OLED_SETUP
+
+// load our own font, with custom QMK logo
+#define OLED_FONT_H "./font.c"
+
+#define OLED_IC OLED_IC_SSD1306 // default
+#define I2C_DRIVER I2CD0
+#define OLED_DISPLAY_128X64
+#define OLED_BRIGHTNESS 196
+#define I2C1_SDA_PIN GP4
+#define I2C1_SCL_PIN GP5
+// #define I2C1_CLOCK_SPEED 400000
+#define OLED_FADE_OUT
+#define OLED_FADE_OUT_INTERVAL 15
+#define OLED_TIMEOUT 60000
+
+#define SPLIT_OLED_ENABLE
--- keyboards/kerberos/rules.mk
+++ keyboards/kerberos/rules.mk
@@ -1,4 +1,11 @@
+
 # ENCODERS_SETUP
 # https://docs.qmk.fm/features/encoders#encoders
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+
+# OLED_SETUP
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
+LTO_ENABLE = yes
